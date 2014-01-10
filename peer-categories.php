b2dc93aa41fef1a8735307879170914a111687ac
coffee2code <coffee2code@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-10 08:17:21 +0000

    * Update to v2.0 * Add c2c_peer_categories() * Deprecate peer_categories() in favor of c2c_peer_categories() * Change default behavior of c2c_peer_categories() to omit all ancestor categories by default, instead of just directly assigned categories * Add optional arg $omit_ancestors to c2c_peer_categories() only omitting direct parent categories and not all ancestor categories * Add filter 'c2c_get_peer_categories_omit_ancestors' * Add filter 'c2c_peer_categories' to support filter invocation method c2c_peer_categories() * Add c2c_get_peer_categories_list() * Deprecate get_peer_categories_list() in favor of c2c_get_peer_categories_list() * Add filter 'c2c_get_peer_categories_list' to support filter invocation method c2c_get_peer_categories_list() * Add c2c_get_peer_categories() * Deprecate get_peer_categories() in favor of c2c_get_peer_categories() * Add filter 'c2c_get_peer_categories' to support filter invocation method c2c_get_peer_categories() * Add filter 'c2c_peer_categories_list' (which also passes $post_id to the hook) * Deprecate filter 'peer_categories' in favor of 'c2c_peer_categories_list' * Fix to use 'parent' instead of 'child_of' in get_categories() calls so only children categories are obtained * Remove harcoded space added after custom separator in c2c_get_peer_categories_list() * Add unit tests * Add Filters section to readme.txt to document all filters * Note compatibility through WP 3.8+ * Drop compatibility with versions of WP older than 3.6 * Update copyright date (2014) * Code and documentation reformatting (spacing, bracing) * Change donate link * Add banner image
    git-svn-id: https://plugins.svn.wordpress.org/peer-categories/tags/2.0@835972 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- peer-categories.php
+++ peer-categories.php
@@ -1,166 +1,250 @@
 <?php
+/**
+ * @package Peer_Categories
+ * @author Scott Reilly
+ * @version 2.0
+ */
 /*
 Plugin Name: Peer Categories
-Version: 1.0
-Plugin URI: http://coffee2code.com/wp-plugins/peer-categories
+Version: 2.0
+Plugin URI: http://coffee2code.com/wp-plugins/peer-categories/
 Author: Scott Reilly
-Author URI: http://coffee2code.com
-Description: Display only the categories that are peer (i.e. share the same category parent) to all lowest-level assigned categories for the specified post.
+Author URI: http://coffee2code.com/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Description: List the categories that are peer (i.e. share the same category parent) to all lowest-level assigned categories for the specified post.
 
-	This plugin provides a template tag which acts a modified version of WordPress's built-in template tag, `the_category()`.  
-	`the_category()` lists all categories directly assigned to the specified post.  `peer_categories()` lists those categories
-	*PLUS* any categories that are peer to those categories and *MINUS* categories that are parents to other assigned
-	categories.  Peer categories are categories that all share the same category parent.
+Compatible with WordPress 3.6 through 3.8+.
 
-	For example, assume your category structure is hierarchical and looks like this:
+=>> Read the accompanying readme.txt file for instructions and documentation.
+=>> Also, visit the plugin's homepage for additional information and updates.
+=>> Or visit: http://wordpress.org/plugins/peer-categories/
 
-	Vegetables
-	|-- Leafy
-	|   |-- Broccoli
-	|   |-- Bok Choy
-	|   |-- Celery
-	|-- Fruiting
-	|   |-- Bell Pepper
-	|   |-- Cucumber
-	|   |-- Pumpkin
-	|-- Podded
-	|   |-- Chickpea
-	|   |-- Lentil
-	|   |-- Soybean
-
-	If you directly assigned the categories "Fruiting" and "Pumpkin" to a post, `peer_categories()` would return a list that consists
-	of: "Bell Pepper", "Cucumber", and "Pumpkin".  Notice that since "Fruiting" was a parent to a directly assigned category, its peers
-	are not included in the list.  If only "Fruiting" were selected as a category, then "Leafy", "Fruiting", and "Podded" would have been
-	listed.
-
-	By default, categories are listed as an HTML list.  The first argment to the template tag allows you to define a custom separator,
-	e.g. to have a simple comma-separated list of peer categories: <?php peer_categories(','); ?>.
-
-	As with categories listed via the_category(), categories that are listed are presented as links to the respective category's
-	archive page.
-
-	Example usage (based on preceeding example):
-
-	<?php peer_categories(); ?>
-	Displays something like:
-	    <ul><li><a href="http://yourblog.com/category/fruiting/bell-pepper">Bell Pepper</a></li>
-	    <li><a href="http://yourblog.com/category/fruiting/cucumber">Cucumber</a></li>
-	    <li><a href="http://yourblog.com/category/fruiting/pumpkin">Pumpkin</a></li></ul>
-
-	<?php peer_categories(','); ?></ul>
-	Displays something like:
-    	<a href="http://yourblog.com/category/fruiting/bell-pepper">Bell Pepper</a>, <a href="http://yourblog.com/category/fruiting/cucumber">Cucumber</a>, <a href="http://yourblog.com/category/fruiting/pumpkin">Pumpkin</a>
-
-Compatible with WordPress 2.5+, 2.6+, 2.7+, 2.8+.
-
-=>> Read the accompanying readme.txt file for more information.  Also, visit the plugin's homepage
-=>> for more information and the latest updates
-
-Installation:
-
-1. Download the file http://coffee2code.com/wp-plugins/peer-categories.zip and unzip it into your 
-wp-content/plugins/ directory.
-2. Activate the plugin through the 'Plugins' admin menu in WordPress
-3. (optional) Add filters for 'peer_categories' to filter peer category listing
-4. Use the template tag <?php peer_categories() ?> somewhere inside "the loop"
+TODO:
+	* Prefix function wit 'c2c_' and deprecate existing versions
+	* Support filter invocation approach via add_filter( 'peer_categories', 'peer_categories', 10, 2 );
+	* Document previously mentioned filters
 
 */
 
 /*
-Copyright (c) 2008-2009 by Scott Reilly (aka coffee2code)
+	Copyright (c) 2008-2014 by Scott Reilly (aka coffee2code)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, 
-modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
-Software is furnished to do so, subject to the following conditions:
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-/*
-	In the loop 
+if ( ! function_exists( 'c2c_peer_categories' ) ) :
+
+/**
+ * Outputs the peer categories.
+ *
+ * For use in the loop
+ *
+ * @since 2.0
+ *
+ * @param  string    $separator (optional) String to use as the separator
+ * @param  int|false $post_id   (optional) Post ID
+ * @return void      (Text is echoed)
 */
-function peer_categories( $separator = '', $post_id = false ) {
-	echo get_peer_categories_list($separator, $post_id);
+function c2c_peer_categories( $separator = '', $post_id = false ) {
+	echo c2c_get_peer_categories_list( $separator, $post_id );
 }
 
-function get_peer_categories_list( $separator = '', $post_id = false ) {
-	global $wp_rewrite;
-	$categories = get_peer_categories($post_id);
-	if ( empty($categories) )
-		return apply_filters('peer_categories', __('Uncategorized'), $separator);
+add_action( 'c2c_peer_categories', 'c2c_peer_categories', 10, 2 );
 
-	$rel = ( is_object($wp_rewrite) && $wp_rewrite->using_permalinks() ) ? 'rel="category tag"' : 'rel="category"';
+endif;
+
+
+if ( ! function_exists( 'c2c_get_peer_categories_list' ) ) :
+
+/**
+ * Gets the list of peer categories.
+ *
+ * @since 2.0
+ *
+ * @param  string $   separator (optional) String to use as the separator
+ * @param  int|false  $post_id (optional) Post ID
+ * @return string     The HTML formatted list of peer categories
+ */
+function c2c_get_peer_categories_list( $separator = '', $post_id = false ) {
+	global $wp_rewrite;
+
+	$categories = c2c_get_peer_categories( $post_id );
+
+	if ( empty( $categories ) ) {
+		return apply_filters(
+			'c2c_peer_categories_list',
+			apply_filters( 'peer_categories', __( 'Uncategorized' ), $separator ), // Deprecated as of v2.0
+			$separator,
+			$post_id
+		);
+	}
+
+	$rel = ( is_object( $wp_rewrite ) && $wp_rewrite->using_permalinks() ) ? 'rel="category tag"' : 'rel="category"';
 
 	$thelist = '';
 	if ( '' == $separator ) {
 		$thelist .= '<ul class="post-categories">';
 		foreach ( $categories as $category ) {
 			$thelist .= "\n\t<li>";
-			$thelist .= '<a href="' . get_category_link($category->term_id) . '" title="' . sprintf(__("View all posts in %s"), $category->name) . '" ' .
-						$rel . '>' . $category->cat_name.'</a></li>';
+			$thelist .= '<a href="' . get_category_link( $category->term_id ) . '" title="' .
+					sprintf( __( 'View all posts in %s' ), $category->name ) . '" ' .
+					$rel . '>' . $category->cat_name . '</a></li>';
 		}
 		$thelist .= '</ul>';
 	} else {
 		$i = 0;
 		foreach ( $categories as $category ) {
-			if ( 0 < $i )
-				$thelist .= $separator . ' ';
-			$thelist .= '<a href="' . get_category_link($category->term_id) . '" title="' . sprintf(__("View all posts in %s"), $category->name) . '" ' . $rel . '>' . $category->name.'</a>';
+			if ( 0 < $i ) {
+				$thelist .= $separator;
+			}
+			$thelist .= '<a href="' . get_category_link( $category->term_id ) . '" title="' .
+					sprintf( __( 'View all posts in %s' ), $category->name ) . '" ' .
+					$rel . '>' . $category->name.'</a>';
 			++$i;
 		}
 	}
-	return apply_filters('peer_categories', $thelist, $separator);
+
+	return apply_filters(
+		'c2c_peer_categories_list',
+		apply_filters( 'peer_categories', $thelist, $separator ), // Deprecated as of v2.0
+		$separator,
+		$post_id
+	);
 }
 
-function get_peer_categories( $id = false ) {
-	$categories = get_the_category($id);
-	if ( empty($categories) ) {
-		return get_categories(array('hide_empty' => false, 'user_desc_for_title' => false, 'title_li' => '', 'child_of' => 0, 'depth' => 1));
+add_filter( 'c2c_get_peer_categories_list', 'c2c_get_peer_categories_list', 10, 2 );
+
+endif;
+
+
+if ( ! function_exists( 'c2c_get_peer_categories' ) ) :
+
+/**
+ * Returns the list of peer categories for the specified post.
+ *
+ * If not supplied a post ID, then the top-level categories will be returned.
+ *
+ * @since 2.0
+ *
+ * @param  int|false $post_id (optional) Post ID
+ * @param  bool      $omit_ancestors (optional) Prevent any ancestors from also being listed, not just immediate parents?
+ * @return array     The array of peer categories for the given category. If false, then assumes a top-level category.
+ */
+function c2c_get_peer_categories( $post_id = false, $omit_ancestors = true ) {
+	$categories = get_the_category( $post_id );
+
+	if ( empty( $categories ) ) {
+		return get_categories(
+			array( 'hide_empty' => false, 'user_desc_for_title' => false, 'title_li' => '', 'parent' => 0, 'exclude' => get_option( 'default_category' ) )
+		);
 	}
 
-	$peers = array();
-	$parents = array();
+	$peers = $parents = array();
+
+	$omit_ancestors = apply_filters( 'c2c_get_peer_categories_omit_ancestors', $omit_ancestors );
 
 	// Go through all categories and get, then filter out, parents.
-	foreach ( $categories as $c ) { $parents[] = $c->parent; }
 	foreach ( $categories as $c ) {
-		if ( !in_array($c->term_id, $parents) ) { $peers[] = $c; }
+		if ( $c->parent && ! in_array( $c->parent, $parents ) ) {
+			if ( $omit_ancestors ) {
+				$parents = array_merge( $parents, get_ancestors( $c->term_id, 'category' ) );
+			} else {
+				$parents[] = $c->parent;
+			}
+		}
 	}
-	
+	$parents = array_unique( $parents );
+
+	foreach ( $categories as $c ) {
+		if ( ! in_array( $c->term_id, $parents ) ) {
+			$peers[] = $c;
+		}
+	}
+
 	// For each cat at this point, get peer cats.
 	$parents = array();
 	foreach ( $peers as $c ) {
-		if ( $c->parent ) {
-			$parents[] = $c->parent;
-		} else {
-			$parents[] = 0;
-		}
+		$parents[] = ( $c->parent ? $c->parent : 0 );
 	}
-	$parents = array_unique($parents);
+	$parents = array_unique( $parents );
+
 	$peers = array();
 	foreach ( $parents as $p ) {
-		$args = array('hide_empty' => false, 'user_desc_for_title' => false, 'title_li' => '', 'child_of' => $p, 'depth' => 0);
-		$cats = get_categories($args);
+		$args = array( 'hide_empty' => false, 'user_desc_for_title' => false, 'title_li' => '', 'parent' => $p );
+		$cats = get_categories( $args );
+
 		# If this cat has no parent, then only get root categories
 		if ( $p == 0 ) {
 			$new_peers = array();
-			foreach ($cats as $c) {
-				//TODO? Might also want to add extra conditional clause of !in_array($c->parent, $parents)
-				if ($c->parent == 0) $new_peers[] = $c;
+			foreach ( $cats as $c ) {
+				if ( $c->parent && ! in_array( $c->parent, $parents ) ) {
+					$new_peers[] = $c;
+				}
 			}
 		} else {
 			$new_peers = $cats;
 		}
-		$peers = array_merge($peers, $new_peers);
+		$peers = array_merge( $peers, $new_peers );
 	}
-	usort($peers, '_usort_terms_by_name');
+	usort( $peers, '_usort_terms_by_name' );
+
 	return $peers;
 }
 
-?>
+add_filter( 'c2c_get_peer_categories', 'c2c_get_peer_categories', 10, 2 );
+
+endif;
+
+
+
+/*************
+ * DEPRECATED FUNCTIONS
+ *************/
+
+
+
+if ( ! function_exists( 'peer_categories' ) ) :
+/**
+ * @since 1.0
+ * @deprecated 2.0 Use c2c_peer_categories() instead
+ */
+function peer_categories( $separator = '', $post_id = false ) {
+	_deprecated_function( 'peer_categories', '2.0', 'c2c_peer_categories' );
+	c2c_peer_categories( $separator, $post_id );
+}
+endif;
+
+if ( ! function_exists( 'get_peer_categories_list' ) ) :
+/**
+ * @since 1.0
+ * @deprecated 2.0 Use c2c_get_peer_categories_list() instead
+ */
+function get_peer_categories_list( $separator = '', $post_id = false ) {
+	_deprecated_function( 'get_peer_categories_list', '2.0', 'c2c_get_peer_categories_list' );
+	return c2c_get_peer_categories_list( $separator, $post_id );
+}
+endif;
+
+if ( ! function_exists( 'get_peer_categories' ) ) :
+/**
+ * @since 1.0
+ * @deprecated 2.0 Use c2c_get_peer_categories() instead
+ */
+function get_peer_categories( $id = false ) {
+	_deprecated_function( 'get_peer_categories', '2.0', 'c2c_get_peer_categories' );
+	return c2c_get_peer_categories( $id );
+}
+endif;
